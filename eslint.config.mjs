ce3e91d47b9ec5948633677883f0c596fc076e1e
Yagunov Denis <spb.jagunov.denis@gmail.com>
2024-12-03 15:36:01 +0300

    Настроена сборка проекта и линтеры

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import airbnb from "eslint-config-airbnb-base";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: ["dist/*"],
+  },
+  {
+    languageOptions: { globals: globals.browser },
+    plugins: {
+      airbnb,
+    },
+  },
+  pluginJs.configs.recommended,
+];
